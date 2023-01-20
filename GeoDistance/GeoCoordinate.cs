a9d2ae0ab1736b3e2b0fb8f276bbd5695a8c3c12
Dima <dima.aidinov@gmail.com>
2023-01-20 20:47:57 +0200

    add geocoordinats

---
+++ GeoDistance/GeoCoordinate.cs
@@ -0,0 +1,34 @@
+﻿namespace GeoDistance
+{
+    public record GeoCoordinate(double Latitude, double Longitude)
+    {
+        public override string ToString()
+        {
+            return $"{Math.Abs(Latitude):G4}{(Latitude >= 0 ? "N" : "S")} {Math.Abs(Longitude):G4}{(Longitude >= 0 ? "E" : "W")}";
+        }
+
+        public double DistanceTo(GeoCoordinate point)
+        {
+            return DistanceBetweenCoordinates(this, point);
+        }
+
+        private const int EarthRadiusKilometers = 6371;
+        private const double Deg2Rad = MathF.PI / 180;
+
+        public static double DistanceBetweenCoordinates(double latitudeA, double longitudeA, double latitudeB, double longitudeB)
+        {
+            var deltaLat = (latitudeB - latitudeA) * Deg2Rad;
+            var deltaLon = (longitudeB - longitudeA) * Deg2Rad;
+            var a = Math.Pow(Math.Sin(deltaLat / 2), 2) +
+                    Math.Cos(latitudeA * Deg2Rad) * Math.Cos(latitudeB * Deg2Rad) *
+                    Math.Pow(Math.Sin(deltaLon / 2), 2);
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return c * EarthRadiusKilometers;
+        }
+
+        public static double DistanceBetweenCoordinates(GeoCoordinate pointA, GeoCoordinate pointB)
+        {
+            return DistanceBetweenCoordinates(pointA.Latitude, pointA.Longitude, pointB.Latitude, pointB.Longitude);
+        }
+    }
+}
--- GeoDistance/Program.cs
+++ GeoDistance/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GeoDistance;
+
+GeoCoordinate Parse(string str)
+{
+    var parts = str.Split(' ');
+    var lat = double.Parse(parts[0].Substring(0, parts[0].Length - 1)) * (parts[0][parts[0].Length - 1] == 'N' ? 1 : -1);
+    var lon = double.Parse(parts[1].Substring(0, parts[1].Length - 1)) * (parts[1][parts[1].Length - 1] == 'E' ? 1 : -1);
+    return new GeoCoordinate(lat, lon);
+}
+
+Console.WriteLine("Enter coordinate A:");
+var input = Console.ReadLine();
+var coordA = Parse(input);
+Console.WriteLine("Enter coordinate B:");
+input = Console.ReadLine();
+var coordB = Parse(input);
+
+Console.WriteLine($"Distance from {coordA} to {coordB} is: {coordA.DistanceTo(coordB):G4}km");
